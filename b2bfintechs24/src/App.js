2da667826b4cebe4ef163fb86a772f138d1a56f7
Rahik-Sikder <sikder.rahik@gmail.com>
2024-03-25 16:30:54 -0500

    Created example pages and components

--- b2bfintechs24/src/App.js
+++ b2bfintechs24/src/App.js
@@ -1,10 +1,24 @@
+import React from "react";
+
+import { Route, Routes } from "react-router-dom";
+
+import Dashboard from './pages/Dashboard'
+import Login from './pages/Login'
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      Hello World
-    </div>
+    <Routes>
+      <Route
+          path="/"
+          element={<Dashboard/>}
+        />
+      <Route
+          path="/login"
+          element={<Login/>}
+        />
+    </Routes>
   );
 }
 
