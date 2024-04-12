f2ec5c32d4a88d9cf238fc3459f56c9b8d0a04da
Rahik-Sikder <sikder.rahik@gmail.com>
2024-04-12 13:35:43 -0500

    added top right display to dashboard

--- b2bfintechs24/src/components/SimplePaper.js
+++ b2bfintechs24/src/components/SimplePaper.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { Paper } from "@mui/material";
 
-const SimplePaper = ({ height, color, padding = 5, children, justifyContent="center"  }) => {
+const SimplePaper = ({ height, color, padding = 5, children, justifyContent="center", width  }) => {
   return (
     <Paper
       sx={{
         minHeight: height,
+        minWidth: width,
         maxWidth: "100%",
         backgroundColor: color,
         padding: padding,
