47b6da32c66fa36151ae24166015af28108a86c5
Rahik-Sikder <sikder.rahik@gmail.com>
2024-04-13 21:50:40 -0500

    added basic designs to app

--- b2b_app/src/Navigation.js
+++ b2b_app/src/Navigation.js
@@ -4,10 +4,25 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 const Stack = createNativeStackNavigator();
 
+import Scan_Page from "./pages/Scan_Page";
+import Input_Page from "./pages/Input_Page";
+
 const Navigator = () => {
+
+  return (
     <NavigationContainer>
-        
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}
+        initialRouteName="Input_Page"
+      >
+        <Stack.Screen name="Scan_Page" component={Scan_Page} />
+        <Stack.Screen name="Input_Page" component={Input_Page} />
+
+      </Stack.Navigator>
     </NavigationContainer>
-}
+  );
+};
 
 export default Navigator;
---
+++ b2b_app/src/pages/Scan_Page.js
@@ -0,0 +1,15 @@
+import { Text, View, Pressable, SafeAreaView, ScrollView } from "react-native";
+
+import PageContainer from "../components/PageContainer";
+
+const Scan_Page = () => {
+  return (
+    <PageContainer>
+      <View>
+        <Text>Scan Page</Text>
+      </View>
+    </PageContainer>
+  );
+};
+
+export default Scan_Page;
