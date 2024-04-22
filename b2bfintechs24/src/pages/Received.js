46d8bed3c8d1a4de7024156aabce1449176e508e
Rahik-Sikder <sikder.rahik@gmail.com>
2024-04-22 02:00:04 -0500

    adding data to recieved

--- b2bfintechs24/src/pages/Received.js
+++ b2bfintechs24/src/pages/Received.js
@@ -1,21 +1,132 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import {
+  Typography,
+  Box,
+  Stack,
+  Button,
+  Table,
+  TableHead,
+  TableRow,
+  TableCell,
+  TableBody,
+  Select,
+  MenuItem,
+  Checkbox,
+  IconButton,
+} from "@mui/material";
 
-import { Typography, Box, Container, Stack } from "@mui/material";
+import Popup from "../components/Popup";
+
+
+import InfoIcon from "@mui/icons-material/Info";
 
 import PageContainer from "../components/PageContainer";
 import SimplePaper from "../components/SimplePaper";
-import { PieChart, Pie, Cell, Tooltip } from "recharts";
+
+import { getRecievedData } from "../api/get-data";
 
 const Received = () => {
-  // Filler data for the pie chart
-  const data = [
-    { name: "Category A", value: 400 },
-    { name: "Category B", value: 300 },
-    { name: "Category C", value: 300 },
-    { name: "Category D", value: 200 },
-  ];
 
-  const COLORS = ["#67C587", "#A9DEBA", "#C9EAD4", "#EAF6ED"];
+  const [numRows, setNumRows] = useState(20);
+  const [selectAll, setSelectAll] = useState(false);
+  const [selectedRows, setSelectedRows] = useState([]);
+  const [pendingData, setPendingData] = useState([]);
+
+  const [showPopUp, setShowPopUp] = useState(false);
+  const [popupInfo, setPopupInfo] = useState({});
+
+  useEffect(() => {
+    async function fetchData() {
+      getRecievedData(setPendingData);
+    }
+    fetchData();
+
+    return () => {
+      // Perform any clean-up here if necessary
+    };
+  }, []);
+
+  const handleChangeRows = (event) => {
+    setNumRows(event.target.value);
+  };
+
+  const handleSelectAll = (event) => {
+    setSelectAll(event.target.checked);
+    if (event.target.checked) {
+      const allRows = [];
+      for (let i = 0; i <= numRows; i++) {
+        allRows.push(i);
+      }
+      setSelectedRows(allRows);
+    } else {
+      setSelectedRows([]);
+    }
+  };
+
+  const handleSelectRow = (event, rowNumber) => {
+    const selectedIndex = selectedRows.indexOf(rowNumber);
+    let newSelected = [];
+
+    if (selectedIndex === -1) {
+      newSelected = newSelected.concat(selectedRows, rowNumber);
+    } else if (selectedIndex === 0) {
+      newSelected = newSelected.concat(selectedRows.slice(1));
+    } else if (selectedIndex === selectedRows.length - 1) {
+      newSelected = newSelected.concat(selectedRows.slice(0, -1));
+    } else if (selectedIndex > 0) {
+      newSelected = newSelected.concat(
+        selectedRows.slice(0, selectedIndex),
+        selectedRows.slice(selectedIndex + 1)
+      );
+    }
+    setSelectedRows(newSelected);
+  };
+
+  const handleClosePopup = () => {
+    setShowPopUp(false);
+  };
+
+  const isSelected = (rowNumber) => selectedRows.indexOf(rowNumber) !== -1;
+
+  const generateRows = () => {
+    // Generate rows based on the number of rows selected
+    const rows = [];
+
+    pendingData.slice(0, numRows).map((order, i) => {
+      console.log("Order is ", order);
+      rows.push(
+        <TableRow
+          key={i}
+          selected={isSelected(i)}
+          onClick={(event) => handleSelectRow(event, i)}
+        >
+          <TableCell padding="checkbox">
+            <Checkbox
+              checked={isSelected(i)}
+              onChange={(event) => handleSelectRow(event, i)}
+            />
+          </TableCell>
+          <TableCell>Order #{order.id}</TableCell>
+          <TableCell>{order.item_name}</TableCell>
+
+          <TableCell align="right"> {order.return_req_date}</TableCell>
+          <TableCell align="right">{order.refund_amount}</TableCell>
+          <TableCell align="right">
+            {/* Gives a warning that I'm passing an object instead of a function. Not sure why. */}
+            <IconButton
+              onClick={() => {
+                setPopupInfo(order);
+                setShowPopUp(true);
+              }}
+            >
+              <InfoIcon color="primary" />
+            </IconButton>
+          </TableCell>
+        </TableRow>
+      );
+    });
+    return rows;
+  };
 
   return (
     <PageContainer>
@@ -24,35 +135,114 @@ const Received = () => {
           Received
         </Typography>
       </Box>
-      <Container maxWidth="lg">
-        <Stack spacing={4} my={5}>
-          <SimplePaper height={400} justifyContent="flex-start" >
-            <PieChart width={400} height={400}>
-              <Pie
-                data={data}
-                cx={200}
-                cy={200}
-                labelLine={false}
-                outerRadius={150}
-                fill="#8884d8"
-                dataKey="value"
-                nameKey="name"
-                isAnimationActive={false} // Disable animation
-              >
-                {data.map((entry, index) => (
-                  <Cell
-                    key={`cell-${index}`}
-                    fill={COLORS[index % COLORS.length]}
-                  />
-                ))}
-              </Pie>
-              <Tooltip />
-            </PieChart>
-          </SimplePaper>
-          <SimplePaper height={500}></SimplePaper>
+      <Stack spacing={2} marginTop={2} position="relative">
+        {/* Select All Checkbox */}
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            marginLeft: "24px",
+          }}
+        ></Box>
+
+        {/* Row Selector */}
+        <Stack
+          spacing={2}
+          direction="row"
+          sx={{ display: "flex", alignItems: "center" }}
+        >
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "center",
+            }}
+          >
+            <Typography
+              variant="body1"
+              color="text.primary"
+              sx={{ marginRight: "10px" }}
+            >
+              Rows:{" "}
+            </Typography>
+            <Select value={numRows} onChange={handleChangeRows}>
+              <MenuItem value={10}>10</MenuItem>
+              <MenuItem value={20}>20</MenuItem>
+              <MenuItem value={30}>30</MenuItem>
+            </Select>
+          </Box>
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              paddingRight: 2,
+            }}
+          >
+            <Checkbox checked={selectAll} onChange={handleSelectAll} />
+            <Typography
+              variant="body1"
+              color="text.primary"
+              sx={{ fontSize: 20, fontFamily: "Rubik", marginLeft: 1 }}
+            >
+              Select All
+            </Typography>
+          </Box>
         </Stack>
-      </Container>
+        {/* Table */}
+        <Box bgcolor="white" borderRadius="4px" overflow="hidden">
+          {" "}
+          {/* Set background color to white */}
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell padding="checkbox">
+                  {/* Placeholder for checkbox */}
+                </TableCell>
+                <TableCell>
+                  <Typography variant="body1" color="text.primary">
+                    Order #
+                  </Typography>
+                </TableCell>
+                <TableCell>
+                  <Typography variant="body1" color="text.primary">
+                    Item Name
+                  </Typography>
+                </TableCell>
+                <TableCell align="right">
+                  <Typography variant="body1" color="text.primary">
+                    Date of Return
+                  </Typography>
+                </TableCell>
+                <TableCell align="right">
+                  <Typography variant="body1" color="text.primary">
+                    Refund Amount
+                  </Typography>
+                </TableCell>
+                <TableCell align="right">
+                  <Typography variant="body1" color="text.primary"></Typography>
+                </TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>{generateRows()}</TableBody>
+          </Table>
+        </Box>
+        {/* Pop-up with order information */}
+        {showPopUp && (
+          <OrderInfoPopup
+            order={popupInfo}
+            handleClosePopup={handleClosePopup}
+          />
+        )}
+        {/* Other components */}
+        {/* <SimplePaper height={200} /> */}
+      </Stack>
     </PageContainer>
+  );
+};
+
+const OrderInfoPopup = ({ order, handleClosePopup }) => {
+  console.log(order);
+  return (
+    <Popup width={1045} height={753} order={order} onClose={handleClosePopup} />
   );
 };
 
