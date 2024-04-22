daa9368752f8d9bb72a0dd3e840c36634e72d506
Rahik-Sikder <sikder.rahik@gmail.com>
2024-04-22 00:29:32 -0500

    connected dashboard to backend

--- b2bfintechs24/src/components/SimplePaper.js
+++ b2bfintechs24/src/components/SimplePaper.js
@@ -1,18 +1,29 @@
 import React from "react";
 import { Paper } from "@mui/material";
 
-const SimplePaper = ({ height, color, padding = 5, children, justifyContent="center", width  }) => {
+const SimplePaper = ({
+  height,
+  color,
+  padding = 5,
+  children,
+  justifyContent = "center",
+  width,
+  textColor,
+  textAlign,
+}) => {
   return (
     <Paper
       sx={{
         minHeight: height,
         minWidth: width,
         maxWidth: "100%",
+        color: textColor,
         backgroundColor: color,
         padding: padding,
         display: "flex",
-        justifyContent: justifyContent, 
+        justifyContent: justifyContent,
         alignItems: "center",
+        textAlign: textAlign
       }}
     >
       {children}
